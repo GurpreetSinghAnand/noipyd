e57930933f64b6600a5659a34be7b069f0d9834e
Gurpreet Singh Anand <gurpreetsinghanand@live.com>
2019-06-25 02:45:12 +0530

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='turtle',
+    version='1.0.1',
+    packages=['turtle'],
+    url='',
+    license='MIT License',
+    author='Gurpreet Singh Anand',
+    author_email='gurpreetsinghanand@live.com',
+    description='To automate NO-IP domain renewal'
+)
